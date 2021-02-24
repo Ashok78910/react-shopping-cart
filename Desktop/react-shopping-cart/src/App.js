4de275eb3cc0cbf497c3e2ec87e33a7a8aa9b11a
Ashok78910 <ak06715272gmail.com>
2021-02-24 16:30:45 +0530

    create profuct with css

--- Desktop/react-shopping-cart/src/App.js
+++ Desktop/react-shopping-cart/src/App.js
@@ -1,23 +1,42 @@
 // feature 1
+import  {Component} from 'react';
 import React from 'react';
+import Data from './data.json';
+import Product from './components/products'
 
-function App() {
-  return (
-    <>
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+       products:Data.products,
+       size : "",
+       sort:""
+    }
+  }
+ render (){
+   return( 
+   <>
     <div className = 'grid-container'>
       <header>
         <a href ='/'>React shopping cart</a>
       </header>
       <main>
-        Product list
+       <div className = 'content'>
+         <div className = 'main'>
+         <Product products = {this.state.products}/>
+         </div>
+         <div className = 'sidebar'>
+           Cart items
+         </div>
+       </div>
       </main>
       <footer>
         All right is reserved
       </footer>
 
     </div>
-    </>
-  );
+    </>)
+  }
 }
 
 export default App;
